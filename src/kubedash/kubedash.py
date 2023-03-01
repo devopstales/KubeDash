3fae5d729c8188b035bae3bf7dda0fc4efd77406
devopstales <42894256+devopstales@users.noreply.github.com>
2023-03-01 19:51:48 +0100

    core: fix selenium test

--- src/kubedash/kubedash.py
+++ src/kubedash/kubedash.py
@@ -76,4 +76,4 @@ def create_app(config_name="development"):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
--- src/kubedash/tests/functional/health_test.py
+++ src/kubedash/tests/functional/health_test.py
@@ -1,4 +1,5 @@
-import pytest, json
+import json, responses
+from flask import url_for
 
 def test_ping(client):
     response = client.get('/ping')
@@ -11,6 +12,16 @@ def test_health(client):
     assert response.status_code == 200
     assert res['health'] == "healthy"
 
+def test_home(client):
+    response = client.get("/")
+    assert b"<title>KubeDash - Login</title>" in response.data
+
+@responses.activate
+def test_invalid_login(client):
+    client.post("/", data={"username": "test", "password": "testpassword"})
+    response = client.get("/dashboard")
+    assert response.status_code == 302
+
 def test_dashboard__not_logged_in(client):
     res = client.get('/dashboard')
     assert res.status_code == 302
@@ -20,3 +31,17 @@ def test_dashboard__logged_in(client):
         client.post("/", data={"username": "pytest", "password": "pytest"}, follow_redirects=True)
         res = client.get('/dashboard')
         assert res.status_code == 200
+
+def test_take_screenshot(live_server, page):
+    page.goto(url_for('main.login', _external=True))
+    page.screenshot(path="screenshots/home.png")
+
+def test_login(live_server, page):
+    page.goto(url_for('main.login', _external=True))
+    page.get_by_placeholder("Username").fill("pytest")
+    page.get_by_placeholder("Password").fill("pytest")
+    page.get_by_role("button", name="Sign in").click()
+
+    page.screenshot(path="screenshots/dashboard.png")
+
+# https://playwright.dev/docs/getting-started-vscode
