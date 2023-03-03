cbb0efc5ffb20fc5afd2fa6e9a32c295b9a2e071
devopstales <42894256+devopstales@users.noreply.github.com>
2023-03-03 15:15:09 +0100

    test: update selenium test

--- src/kubedash/tests/conftest.py
+++ src/kubedash/tests/conftest.py
@@ -5,13 +5,12 @@ from kubedash import create_app
 @pytest.fixture(scope='module')
 def app():
     app = create_app("testing")
-    #with app.app_context():
     yield app
 
 @pytest.fixture(scope='module')
 def client(app):
-    #ctx = app.test_request_context()
-    #ctx.push()
-    #app.test_client_class = FlaskClient
+    ctx = app.test_request_context()
+    ctx.push()
+    app.test_client_class = FlaskClient
     return app.test_client()
     
