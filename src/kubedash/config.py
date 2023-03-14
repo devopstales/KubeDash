ab0971aad66d6288897a50a16b86779c5136a4c7
devopstales <42894256+devopstales@users.noreply.github.com>
2023-03-14 11:20:59 +0100

    fix: database init with flask_migrate

--- src/kubedash/config.py
+++ src/kubedash/config.py
@@ -1,9 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     """
     Common configurations
     """
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
@@ -17,21 +20,21 @@ class DevelopmentConfig(Config):
     Development configurations
     """
     #SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+basedir+"/database/development.db"
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///production.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+basedir+"/database/production.db"
 
 class TestingConfig(Config):
     """
     Testing configurations
     """
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+basedir+"/database/testing.db"
     WTF_CSRF_ENABLED = False
 
 app_config = {
