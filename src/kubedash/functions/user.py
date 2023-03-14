4036224e1d20ffb62d33583a7cab839bb102d7d4
devopstales <42894256+devopstales@users.noreply.github.com>
2023-03-14 15:25:08 +0100

    feat!: ganerate kubectl config at user create #6
    Signed-off-by: devopstales <42894256+devopstales@users.noreply.github.com>

--- src/kubedash/functions/user.py
+++ src/kubedash/functions/user.py
@@ -33,6 +33,8 @@ class User(UserMixin, db.Model):
                             backref=db.backref('users', lazy='dynamic'))
     user_type = db.Column(db.String(5), nullable=False)
     tokens = db.Column(db.Text, nullable=True)
+    kubectl_config = db.relationship('kubectlConfig', secondary='users_kubectl',
+                            backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -61,35 +63,39 @@ def UserTest(username):
     user = User.query.filter_by(username=username).first()
     return user
 
-def UserCreate(username, password, email, user_type, role=None, tokens=None):
+def UserCreate(username, password, email, user_type, role=None, tokens=None, kubectl_config=None):
     user = UserTest(username)
     if not user:
         if password is None:
             user = User(
-                username      = username,
-                password_hash = None,
-                email         = email,
-                user_type     = user_type,
-                tokens        = tokens
+                username       = username,
+                password_hash  = None,
+                email          = email,
+                user_type      = user_type,
+                tokens         = tokens,
             )
         else:
             user = User(
-                username      = username,
-                password_hash = generate_password_hash(password, method='sha256'),
-                email         = email,
-                user_type     = user_type,
-                tokens        = tokens
+                username       = username,
+                password_hash  = generate_password_hash(password, method='sha256'),
+                email          = email,
+                user_type      = user_type,
+                tokens         = tokens,
             )
         if role:
             role_data = Role.query.filter(Role.name == role).first()
             user.roles.append(role_data)
+        if kubectl_config:
+            kubectl = kubectlConfig.query.filter(kubectlConfig.name == kubectl_config).first()
+            user.kubectl_config.append(kubectl)
         db.session.add(user)
         db.session.commit()
 
-def UserUpdate(username, role):
+def UserUpdate(username, role, user_type):
     user = User.query.filter_by(username=username).first()
     if user:
         user.role = role
+        user.user_type = user_type
         db.session.commit()
 
 def UserDelete(username):
@@ -109,3 +115,29 @@ def UserUpdatePassword(username, password):
             return True
         else:
             return False
+        
+# Define the kubectlConfig data model
+class kubectlConfig(db.Model):
+    __tablename__ = 'kubectl_config'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), nullable=False, server_default=u'', unique=False)
+    private_key = db.Column(db.Text, nullable=True)
+    user_certificate = db.Column(db.Text, nullable=True)
+
+# Define the UsersKubectl association model
+class UsersKubectl(db.Model):
+    __tablename__ = 'users_kubectl'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('kubectl_config.id', ondelete='CASCADE'))
+
+def KubectlConfigStore(name, private_key_base64, user_certificate_base64):
+    kubectl = kubectlConfig.query.filter(kubectlConfig.name == name).first()
+    if not kubectl:
+        kubectl_config = kubectlConfig(
+            name = name,
+            private_key = private_key_base64,
+            user_certificate = user_certificate_base64,
+        )
+        db.session.add(kubectl_config)
+        db.session.commit()
