171d5fc7653ab71f39819e50d5b899f2c0174480
devopstales <42894256+devopstales@users.noreply.github.com>
2023-07-22 13:23:00 +0200

    fix: minor fixes

--- src/kubedash/run.sh
+++ src/kubedash/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#export FLASK_DEBUG=1
-#export TEMPLATES_AUTO_RELOAD=1
+export FLASK_DEBUG=1
+export TEMPLATES_AUTO_RELOAD=1
 #export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-export FLASK_CONFIG="production"
+#export FLASK_CONFIG="production" # redirect to https
 export FLASK_APP="kubedash"
 
 USER=$(id -u)
@@ -13,6 +13,8 @@ export USER=$USER
 echo "Start Migration"
 flask db upgrade
 
+mkdir -p /tmp/kubedash
+
 echo "Start Gunicorn"
-#flask run --host=0.0.0.0 --port=8000
-gunicorn --worker-class eventlet --conf gunicorn_conf.py kubedash:app
+flask run --host=127.0.0.1 --port=8000
+#gunicorn --worker-class eventlet --conf gunicorn_conf.py kubedash:app
