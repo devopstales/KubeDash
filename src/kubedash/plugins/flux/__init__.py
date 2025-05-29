5240debaa6626889c50d4bf82b6e63a218d63661
devopstales <42894256+devopstales@users.noreply.github.com>
2025-05-29 09:44:59 +0200

    add(plugin): flux plugin skeleton

--- src/kubedash/plugins/flux/__init__.py
+++ src/kubedash/plugins/flux/__init__.py
@@ -23,3 +23,35 @@ logger = get_logger()
 ##############################################################
 # Flux sources
 ##############################################################
+
+@flux.route("/flux-objects", methods=['GET', 'POST'])
+def get_flux_objects():
+    user_token = get_user_token(session)
+
+    if request.method == 'POST':
+        if request.form.get('ns_select', None):
+            session['ns_select'] = request.form.get('ns_select')
+            
+    results = {}
+    errors = {}
+
+    flux_getters = {
+        "HelmReleases": FluxHelmReleaseGet(session['user_role'], user_token, session['ns_select']),
+        "Kustomizations": FluxKustomizationGet(session['user_role'], user_token, session['ns_select']),
+        "Alerts": FluxAlertNotificationGet(session['user_role'], user_token, session['ns_select']),
+        "Providers": FluxProviderNotificationGet(session['user_role'], user_token, session['ns_select']),
+        "Receivers": FluxReceiverNotificationGet(session['user_role'], user_token, session['ns_select']),
+        "Buckets": FluxBucketRepositoryGet(session['user_role'], user_token, session['ns_select']),
+        "GitRepositories": FluxGitRepositoryGet(session['user_role'], user_token, session['ns_select']),
+        "HelmRepositories": FluxHelmRepositoryGet(session['user_role'], user_token, session['ns_select']),
+        "OCIRepositories": FluxOCIRepositoryGet(session['user_role'], user_token, session['ns_select']),
+    }
+
+    flux_objects = {}
+    for kind, func in flux_getters.items():
+        try:
+            flux_objects[kind] = func()
+        except Exception as e:
+            flux_objects[kind] = []
+
+    return render_template("flux_objects.html.j2", flux_objects=flux_objects)
